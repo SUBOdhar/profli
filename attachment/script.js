158ed834c3a93bb9ab0c8a7052da5a34d9f70e21
FUNCODE <89914933+SUBOdhar@users.noreply.github.com>
2023-05-26 20:34:02 +0545

    ok

--- attachment/script.js
+++ attachment/script.js
@@ -2,3 +2,7 @@ var loader = document.getElementById("preload");   // preloader start
 window.addEventListener("load",function(){
     loader.style.display = "none";
 })  // preloader end
+
+document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+ });
